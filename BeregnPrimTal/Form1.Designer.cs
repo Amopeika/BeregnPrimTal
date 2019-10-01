5d0efa4c2682e59556f8a56093ff5f25d861d7dd
Kevin Pike Darmer <amopeika@gmail.com>
2019-10-01 13:42:21 +0200

    Add project files.

---
+++ BeregnPrimTal/Form1.Designer.cs
@@ -0,0 +1,164 @@
+﻿namespace BeregnPrimTal
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtBegin = new System.Windows.Forms.TextBox();
+            this.txtEnd = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnTest = new System.Windows.Forms.Button();
+            this.lbPrime = new System.Windows.Forms.ListBox();
+            this.progressBarPrime = new System.Windows.Forms.ProgressBar();
+            this.lblTime = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.btnCancel = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtBegin
+            // 
+            this.txtBegin.Location = new System.Drawing.Point(102, 33);
+            this.txtBegin.Name = "txtBegin";
+            this.txtBegin.Size = new System.Drawing.Size(100, 20);
+            this.txtBegin.TabIndex = 0;
+            // 
+            // txtEnd
+            // 
+            this.txtEnd.Location = new System.Drawing.Point(102, 83);
+            this.txtEnd.Name = "txtEnd";
+            this.txtEnd.Size = new System.Drawing.Size(100, 20);
+            this.txtEnd.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 39);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(22, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Fra";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(16, 89);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(18, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Til";
+            // 
+            // btnTest
+            // 
+            this.btnTest.Location = new System.Drawing.Point(38, 127);
+            this.btnTest.Name = "btnTest";
+            this.btnTest.Size = new System.Drawing.Size(99, 41);
+            this.btnTest.TabIndex = 4;
+            this.btnTest.Text = "Start test";
+            this.btnTest.UseVisualStyleBackColor = true;
+            this.btnTest.Click += new System.EventHandler(this.btnTest_Click);
+            // 
+            // lbPrime
+            // 
+            this.lbPrime.FormattingEnabled = true;
+            this.lbPrime.Location = new System.Drawing.Point(66, 194);
+            this.lbPrime.Name = "lbPrime";
+            this.lbPrime.Size = new System.Drawing.Size(135, 303);
+            this.lbPrime.TabIndex = 5;
+            // 
+            // progressBarPrime
+            // 
+            this.progressBarPrime.Location = new System.Drawing.Point(12, 562);
+            this.progressBarPrime.Name = "progressBarPrime";
+            this.progressBarPrime.Size = new System.Drawing.Size(260, 23);
+            this.progressBarPrime.TabIndex = 6;
+            // 
+            // lblTime
+            // 
+            this.lblTime.AutoSize = true;
+            this.lblTime.Location = new System.Drawing.Point(19, 519);
+            this.lblTime.Name = "lblTime";
+            this.lblTime.Size = new System.Drawing.Size(30, 13);
+            this.lblTime.TabIndex = 7;
+            this.lblTime.Text = "Time";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(55, 519);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(35, 13);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "label3";
+            // 
+            // btnCancel
+            // 
+            this.btnCancel.Location = new System.Drawing.Point(143, 127);
+            this.btnCancel.Name = "btnCancel";
+            this.btnCancel.Size = new System.Drawing.Size(99, 41);
+            this.btnCancel.TabIndex = 9;
+            this.btnCancel.Text = "Cancel";
+            this.btnCancel.UseVisualStyleBackColor = true;
+            this.btnCancel.Click += new System.EventHandler(this.btnCancel_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 614);
+            this.Controls.Add(this.btnCancel);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.lblTime);
+            this.Controls.Add(this.progressBarPrime);
+            this.Controls.Add(this.lbPrime);
+            this.Controls.Add(this.btnTest);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtEnd);
+            this.Controls.Add(this.txtBegin);
+            this.Name = "Form1";
+            this.Text = "Test for Prim-tal";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtBegin;
+        private System.Windows.Forms.TextBox txtEnd;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnTest;
+        private System.Windows.Forms.ListBox lbPrime;
+        private System.Windows.Forms.ProgressBar progressBarPrime;
+        private System.Windows.Forms.Label lblTime;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button btnCancel;
+    }
+}
+
---
+++ BeregnPrimTal/Form1.cs
@@ -0,0 +1,124 @@
+﻿// Ref.: http://www.dotnetperls.com/prime
+
+using System;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BeregnPrimTal
+{
+    public partial class Form1 : Form
+    {
+        SynchronizationContext ctx = null;
+        Stopwatch stopwatch = new Stopwatch();
+        CancellationTokenSource tcs = new CancellationTokenSource();
+
+        CancellationToken ct;
+        public Form1()
+        {
+            InitializeComponent();
+            txtBegin.Text = "1";
+            txtEnd.Text = "1000000";
+            ct = tcs.Token;
+        }
+
+        private void btnTest_Click(object sender, EventArgs e)
+        {
+            
+            Parameters param = new Parameters();
+            ctx = SynchronizationContext.Current;
+            stopwatch.Start();
+
+            param.startNumber = Convert.ToInt32(txtBegin.Text);
+            param.endNumber = Convert.ToInt32(txtEnd.Text);
+            progressBarPrime.Minimum = param.startNumber;
+            progressBarPrime.Maximum = param.endNumber;
+
+            //Almindelig Thread
+            //Thread t = new Thread(TestPrimeNumbers);
+            //t.IsBackground = true;
+            //t.Start(param);
+
+            //Thread Pool
+            //ThreadPool.QueueUserWorkItem(TestPrimeNumbers, param);
+
+            //Task with Factory.StartNew
+            //Task t = Task.Factory.StartNew(TestPrimeNumbers, param);
+
+            //Task with start
+            //Task t = new Task(TestPrimeNumbers, param);
+            //t.Start();
+
+            //Task with run
+            //Task.Run(() => TestPrimeNumbers(param,ct));
+
+            //async and await
+
+            //progress 
+            
+            //cancellation
+
+            //http client
+
+        }
+
+        void TestPrimeNumbers(object data, CancellationToken ct)
+        {
+            Parameters param = (Parameters)data;
+            ctx.Send(state => this.Text = "Test igang!", null);
+            for (int i = param.startNumber; i < param.endNumber; i++)
+            {
+                ct.ThrowIfCancellationRequested();
+                bool prime = IsPrime(i);
+                if (prime)
+                {
+                    ctx.Send(state => lbPrime.Items.Add(i), null);
+                }
+                ctx.Send(state => progressBarPrime.Value = i, null);
+            }
+            ctx.Send(state => this.Text = "Test færdig!", null);
+
+            stopwatch.Stop();
+            ctx.Send(state => label3.Text = Convert.ToString(stopwatch.ElapsedMilliseconds), null);
+        }
+        #region Prime test
+        public static bool IsPrime(int candidate)
+        {
+            // Test whether the parameter is a prime number.
+            if ((candidate & 1) == 0)
+            {
+                if (candidate == 2)
+                {
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+
+            for (int i = 3; (i * i) <= candidate; i += 2)
+            {
+                if ((candidate % i) == 0)
+                {
+                    return false;
+                }
+            }
+            return candidate != 1;
+        }
+        #endregion
+
+        private void btnCancel_Click(object sender, EventArgs e)
+        {
+            tcs.Cancel();
+        }
+    }
+    class Parameters
+    {
+        public int startNumber { get; set; }
+
+        public int endNumber { get; set; }
+    }
+}
